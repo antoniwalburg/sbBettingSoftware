c3d43e4ea539d06765c62693dc85758f62e10ac9
Antoni Walburg <103273798+antoniwalburg@users.noreply.github.com>
2022-07-20 17:14:40 +0200

    20.07

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ const groupBy = (items, key) => items.reduce(
             item,],
     }), {})
 
-let iforbetApiUrl = ("http://api.iforbet.pl/rest/market/categories");
+const iforbetApiUrl = ("http://api.iforbet.pl/rest/market/categories");
 let obj;
 fetch(iforbetApiUrl)
     .then(res => res.json())
+
     .then(data => obj = data)
 
     //error handle
@@ -30,12 +31,15 @@ fetch(iforbetApiUrl)
     })
 
     .then(function () {
+
         const { data } = obj
         sortedData = groupBy(data, "sportName")
         keysSortedData = Object.keys(sortedData)
         function sortCategoryNamesByLevel(cateogryLevel) {
+
             const invalidSportId = 18;
             return data.filter(function (e) {
+
                 return e.level == cateogryLevel && (!e.sportName.includes("Esport")) && (e.sportId != invalidSportId)
                 && e.sportId != (invalidSportId * 2) && e.categoryName != "Outrights";
             });
@@ -57,7 +61,10 @@ fetch(iforbetApiUrl)
                 }
             })
         }
+        const dropDownAnimation = "dropdownAnimation"
+        const hideAnimation = "hideAnimation"
         //LEVEL_1
+        const caretString = " »"
         let listLevelOne = document.createElement("ul");
         listLevelOne.setAttribute('class','navbar-sports')
         let categoryNameLevel1 = sortCategoryNamesByLevel(1);
@@ -66,9 +73,12 @@ fetch(iforbetApiUrl)
             const { categoryName, sportId, sportName } = element;
             divone.dataset.sportId = sportId;
             let itemLevelOne = document.createElement("li");
-            itemLevelOne.innerText = categoryName + " »";
+            let spanCaret = document.createElement("span");
+            spanCaret.setAttribute('class','caret')
+            itemLevelOne.innerText = categoryName + caretString;
             itemLevelOne.dataset.sportId = sportId
-            divone.appendChild(itemLevelOne);
+            itemLevelOne.append(spanCaret)
+            divone.appendChild(itemLevelOne);          
             listLevelOne.appendChild(divone)
 
         });
@@ -76,30 +86,51 @@ fetch(iforbetApiUrl)
         document.getElementById("sportsDiv").appendChild(listLevelOne);
         let categoryNameLevel2 = sortCategoryNamesByLevel(2);
             categoryNameLevel2.forEach(element => {
-            const { categoryName, parentCategory, categoryId, sportId, level} = element;
-            let listLevelTwo = document.createElement("ul");
-            listLevelTwo.dataset.sportId = sportId;
-            listLevelTwo.dataset.level = level;
-            listLevelTwo.dataset.categoryId = categoryId;
-            listLevelTwo.setAttribute('class','courses2')
-            
-            let itemLevelTwo = document.createElement("li");
-            itemLevelTwo.innerText = categoryName + " »";
-            itemLevelTwo.dataset.categoryId = categoryId
-            itemLevelTwo.dataset.level = level;
-            listLevelTwo.appendChild(itemLevelTwo);
-            document.querySelector(`[data-sport-id='${parentCategory}'`).appendChild(listLevelTwo)
-        });
+                const { categoryName, parentCategory, categoryId, sportId, level} = element;
+                let listLevelTwo = document.createElement("ul");
+                listLevelTwo.dataset.sportId = sportId;
+                listLevelTwo.dataset.level = level;
+                listLevelTwo.dataset.categoryId = categoryId;
+                listLevelTwo.setAttribute('class','courses2')
+          
+                let itemLevelTwo = document.createElement("li");
+                itemLevelTwo.innerText = categoryName + " »";
+                itemLevelTwo.dataset.categoryId = categoryId
+                itemLevelTwo.dataset.level = level;
+                listLevelTwo.appendChild(itemLevelTwo);
+                document.querySelector(`[data-sport-id='${parentCategory}'`).appendChild(listLevelTwo)
+            });
 
         categoryNameLevel1 = sortCategoryNamesByLevel(1);
         categoryNameLevel1.forEach(element => {
+
             const { categoryName, parentCategory, categoryId, sportId, level} = element;
             const event = document.querySelector(`li[data-sport-id='${sportId}']`)
+            const timeout = 650;
             event.addEventListener('click', (parametr)=>{
+
                 let courses2 = document.querySelectorAll(`ul[data-sport-id='${sportId}'][data-level = '2']`);
+                
                 courses2.forEach(function(item, index, arr){
-                    let checkStyleValue = item.style.display == "block" 
-                    ? item.style.display = "none" : item.style.display = "block";
+
+                    if (item.style.display == "block") {
+    
+                        item.classList.toggle(hideAnimation)
+                        setTimeout(function() {
+
+                            item.style.display = "none";
+                        },timeout)
+
+                        setTimeout(function() {
+                            item.classList.remove(hideAnimation)
+                            item.classList.remove(dropDownAnimation)
+                        },timeout)
+
+                    } else {
+                        item.classList.toggle(dropDownAnimation);
+                        item.style.display = "block";     
+                    }
+
                 })
             })
         });
@@ -107,30 +138,51 @@ fetch(iforbetApiUrl)
         let categoryNameLevel3 = sortCategoryNamesByLevel(3);
         categoryNameLevel3.forEach(element => {
             
-        const { categoryName, parentCategory, categoryId, sportId, level } = element
-        let listLevelThree = document.createElement("ul");
-        listLevelThree.dataset.sportId = sportId;
-        listLevelThree.dataset.level = level;
-        listLevelThree.dataset.categoryId = categoryId;
-        listLevelThree.dataset.parentCategory = parentCategory;
-        listLevelThree.setAttribute('id', categoryId);
-        listLevelThree.setAttribute('class','courses3');  
+            const { categoryName, parentCategory, categoryId, sportId, level } = element
+            let listLevelThree = document.createElement("ul");
+            listLevelThree.dataset.sportId = sportId;
+            listLevelThree.dataset.level = level;
+            listLevelThree.dataset.categoryId = categoryId;
+            listLevelThree.dataset.parentCategory = parentCategory;
+            listLevelThree.setAttribute('id', categoryId);
+            listLevelThree.setAttribute('class','courses3');  
 
-        let itemLevelThree = document.createElement("li");
-        itemLevelThree.innerText = categoryName;
-        itemLevelThree.dataset.categoryId = categoryId
-        listLevelThree.appendChild(itemLevelThree);
-        document.querySelector(`[data-category-id='${parentCategory}'`).appendChild(listLevelThree);
-        });
+            let itemLevelThree = document.createElement("li");
+            itemLevelThree.innerText = categoryName;
+            itemLevelThree.dataset.categoryId = categoryId
+            listLevelThree.appendChild(itemLevelThree);
+            document.querySelector(`[data-category-id='${parentCategory}'`).appendChild(listLevelThree);
+            });
+
         categoryNameLevel2 = sortCategoryNamesByLevel(2);
         categoryNameLevel2.forEach(element => {
+
                 const { categoryName, parentCategory, categoryId, sportId } = element;
-                const event2 = document.querySelector(`ul[data-category-id='${categoryId}']`)
+                const event2 = document.querySelector(`li[data-category-id='${categoryId}']`)
                 event2.addEventListener('click', (parametr)=>{
+
                     let courses3 = document.querySelectorAll(`ul[data-parent-category='${categoryId}'][data-level = '3']`);
+                    const timeout = 650;
                     courses3.forEach(function(item, index, arr){
-                        let checkStyleValue = item.style.display == "block" 
-                        ? item.style.display = "none" : item.style.display = "block";
+                        
+                        if (item.style.display == "block") {
+                            
+                            item.classList.toggle(hideAnimation)
+                            setTimeout(function() {
+
+                                item.style.display = "none";
+                            },timeout)
+    
+                            setTimeout(function() {
+                                item.classList.remove(hideAnimation)
+                                item.classList.remove(dropDownAnimation)
+                            },timeout)
+
+                        } else {
+                            
+                            item.classList.toggle(dropDownAnimation);
+                            item.style.display = "block";
+                        }
                     });
                 
                 })
@@ -138,3 +190,4 @@ fetch(iforbetApiUrl)
 })
 //FILTER_WITH_OUT => ELEMENTS WITH-OUT PAR-CHILD
 //FINAL
+//ERROR ON HIDE-DROP LEVELS1-2 + ERROR ON MULTI CLICK LEVEL 1-3
